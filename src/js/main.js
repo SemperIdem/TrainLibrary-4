0cddf6fdbde924dfbcd6aff30123497c636798d9
SemperIdem <faintler915@gmail.com>
2020-09-23 13:14:52 +0400

    feat: add FadeToggle

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 import $ from './lib/lib';
 
-$('.active').toggle().toggle();
+$('#first').onClick(() => {
+    $('div').eq(1).fadeToggle(800);
+});
+
+$('[data-count="second"]').onClick(() => {
+    $('div').eq(1).fadeIn(800);
+});
+
+$('button').eq(2).onClick(() => {
+    $('.w-500').fadeOut(800);
+})

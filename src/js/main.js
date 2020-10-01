c34b6aab31ef09d742ccba2f6882e893aff8c1e3
SemperIdem <faintler915@gmail.com>
2020-10-01 12:38:28 +0400

    feat: final fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,1 @@
 import $ from './lib/lib';
-
-$('#first').onClick(() => {
-    $('div').eq(1).fadeToggle(800);
-});
-
-$('[data-count="second"]').onClick(() => {
-    $('div').eq(1).fadeIn(800);
-});
-
-$('button').eq(2).onClick(() => {
-    $('.w-500').fadeOut(800);
-})
